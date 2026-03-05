d52a990edd9a3baff24b264195c07b5a3f35cb32
joon363 <joon363@naver.com>
2026-03-05 20:35:08 +0900

    [HW1] integer functions

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -1,11 +1,37 @@
 exception Not_implemented
+exception Invariant
 
 type 'a tree = Leaf of 'a | Node of 'a tree * 'a * 'a tree
 
-let rec fac _ = raise Not_implemented
-let rec power _ _ = raise Not_implemented
-let rec gcd _ _ = raise Not_implemented
-let rec combi _ _ = raise Not_implemented
+let rec fac n = 
+    if n<0 then raise Invariant  
+    (*Stop condition: 1!=1*) 
+    else if n=1 then 1
+    else n * fac (n-1)
+        
+let rec power x n = 
+    if n<0 then raise Invariant
+    (*Stop condition: x^0=1*) 
+    else if n=0 then 1
+    else x * power x (n-1)
+
+let rec gcd m n = 
+    if n<0 || m<0 || m+n<1 then raise Invariant
+    (*Euclid's algorithm
+    for m>n, if m=nq+r then gcd m n = gcd n r*)
+    else let rec inner a b = 
+        (*Stop condition: reached r or r=0*)
+        if a <= b then a 
+        else inner (a-b) b in
+    if m>n then inner m n else inner n m
+        
+let rec combi n k =
+    if n<1 || k<0 || n<k then raise Invariant
+    (*Stop Condition: nCn = 1, nCk = 1*)
+    else if n=k || k=0 then 1 
+    (*Pascal's triangle; nCk = n-1Ck-1 + n-1Ck*)
+    else combi (n-1) (k-1) + combi (n-1) k 
+
 
 let rec sum_tree _ = raise Not_implemented
 let rec depth _ = raise Not_implemented
